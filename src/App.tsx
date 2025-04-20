9d440aea819c5d31d2282a41e5702b7346d5cdcb
Simon Norberg <simon@pthread.se>
2025-04-20 21:59:09 +0200

    Fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from "react"; // Import React explicitly
+import { useState, useEffect, useCallback, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
